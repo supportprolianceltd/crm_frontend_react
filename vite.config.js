043b12d7746f12f32d8d250386d153db7295bb73
Prince Godson <princegodson24@gmail.com>
2025-06-11 20:03:17 +0100

    Fix Vercel 404 by adding rewrite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
   server: {
-    historyApiFallback: true, // Important for dev
+    historyApiFallback: true
   }
-});
+})
