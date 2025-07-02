9ee808e3d876ac37eaa37ae54245e517ed0b82cb
support@prolianceltd.com <support@prolianceltd.com>
2025-07-02 14:35:27 +0100

    Ekene please do onboarding

--- src/config.jsx
+++ src/config.jsx
@@ -1,11 +1,9 @@
 const config = {
 
 // API_BASE_URL: 'http://127.0.0.1:9090',
-// API_BASE_URL: 'https://2fbe-102-90-98-83.ngrok-free.app',
-// API_BASE_URL: 'https://ef6c-102-90-116-15.ngrok-free.app/',
 API_BASE_URL: 'https://cmvp-api-v1.onrender.com',
+
 // WEB_PAGE__URL: 'http://localhost:5173',
-// WEB_PAGE__URL: 'https://370a-102-90-98-83.ngrok-free.app',
 
 WEB_PAGE__URL : 'https://crm-frontend-react.vercel.app',
 
